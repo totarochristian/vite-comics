642a63b0887b0e7b419c8e803802bc5e37b62cf6
totarochristian <totaro.christian.18@gmail.com>
2023-04-13 17:57:59 +0200

    Add the brands text in the brands component

--- src/components/subAreas/BrandsComponent.vue
+++ src/components/subAreas/BrandsComponent.vue
@@ -1,6 +1,10 @@
 <template>
-  <div id="brands" class="w-100 bg-primary">
-
+  <div id="brands" class="w-100 h-100 bg-primary text-white d-flex justify-content-center align-items-center">
+    <div class="w-75 h-100 d-flex justify-content-between align-items-center gap-4">
+      <div class="footerBrandElement h-100" v-for="(elem,index) in brandsElements">
+        <span class="h-100 fw-semibold text-uppercase">{{ elem.text }}</span>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -12,6 +16,9 @@
       return{
         brandsElements: brandsData
       }
+    },
+    methods(){
+
     }
   }
 </script>
