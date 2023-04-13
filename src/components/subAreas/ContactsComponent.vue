c477c2244a997f2d23a15ad404c6874de0d75cd9
totarochristian <totaro.christian.18@gmail.com>
2023-04-13 19:02:36 +0200

    Add sign up btn in the contacts component

--- src/components/subAreas/ContactsComponent.vue
+++ src/components/subAreas/ContactsComponent.vue
@@ -1,6 +1,8 @@
 <template>
-  <div id="contacts" class="w-100 bg-warning">
-
+  <div id="contacts" class="w-100 text-white d-flex justify-content-center align-items-center overflow-hidden">
+    <div class="w-75 d-flex justify-content-between align-items-center gap-4">
+      <button type="button" class="btn btn-outline-primary text-white text-uppercase fw-bold rounded-0 btn-lg">sign-up now!</button>
+    </div>
   </div>
 </template>
 
