85748e22962ee8f8ed9091ab19f634a17ff28935
totarochristian <totaro.christian.18@gmail.com>
2023-04-13 18:35:58 +0200

    Set the font size of the a links in the sites component

--- src/components/subAreas/SitesComponent.vue
+++ src/components/subAreas/SitesComponent.vue
@@ -4,7 +4,7 @@
       <div class="footerSitesElements w-50 d-flex flex-column align-items-start" v-for="(elem,index) in sitesElements">
         <h4 class="h-100 fw-semibold text-uppercase">{{ elem.title }}</h4>
         <div class="footerSitesLinks d-flex flex-column">
-          <a :href="link.link" v-for="link in elem.links">{{ link.text }}</a>
+          <a class="fs-6" :href="link.link" v-for="link in elem.links">{{ link.text }}</a>
         </div>
       </div>
       <img src="../../assets/images/dc-logo-bg.png" alt="Logo DC">
