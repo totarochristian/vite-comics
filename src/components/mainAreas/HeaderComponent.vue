6e24af895dfaec47c3d0f1e3c6b3378d94852cb8
totarochristian <totaro.christian.18@gmail.com>
2023-04-13 19:28:17 +0200

    Add logo component and use it in the header component

--- src/components/mainAreas/HeaderComponent.vue
+++ src/components/mainAreas/HeaderComponent.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="w-100 h-100 bg-white d-flex justify-content-center align-items-center">
     <div class="w-75 h-100 d-flex justify-content-between align-items-center">
-      <img src="../../assets/images/dc-logo.png" alt="Logo DC">
+      <LogoComponent />
       <div id="headerMenu" class="h-100 d-flex justify-content-between align-items-center gap-4">
         <div class="headerMenuElement h-100" v-for="(elem,index) in menuElements" :class="{'active' : elem.isActive}">
           <span class="h-100 fw-semibold text-uppercase">{{ elem.text }}</span>
@@ -13,8 +13,12 @@
 
 <script>
   import { menuData } from '../../data/data';
+  import LogoComponent from '../single/LogoCOmponent.vue';
   export default {
     name: "HeaderComponent",
+    components:{
+      LogoComponent
+    },
     data(){
       return{
         menuElements: menuData
@@ -27,10 +31,6 @@
   @import 'bootstrap/scss/bootstrap';
   header{
     height: 15vh;
-  }
-  header>div>img{
-    height: 80px;
-    width: 80px;
   }
   #headerMenu{
     line-height: 113px;
---
+++ src/components/single/LogoComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <img id="logo" src="../../assets/images/dc-logo.png" alt="Logo DC">
+</template>
+
+<script>
+  export default {
+    name: "LogoComponent"
+  }
+</script>
+
+<style lang="scss" scoped>
+  #logo{
+    height: 80px;
+    width: 80px;
+  }
+</style>
