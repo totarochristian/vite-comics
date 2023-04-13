4c90383ae5faf9234a5ad5a4ba1e7c984f434184
totarochristian <totaro.christian.18@gmail.com>
2023-04-13 19:34:55 +0200

    Add navigation bar component and use it in the header component

--- src/components/mainAreas/HeaderComponent.vue
+++ src/components/mainAreas/HeaderComponent.vue
@@ -2,47 +2,26 @@
   <header class="w-100 h-100 bg-white d-flex justify-content-center align-items-center">
     <div class="w-75 h-100 d-flex justify-content-between align-items-center">
       <LogoComponent />
-      <div id="headerMenu" class="h-100 d-flex justify-content-between align-items-center gap-4">
-        <div class="headerMenuElement h-100" v-for="(elem,index) in menuElements" :class="{'active' : elem.isActive}">
-          <span class="h-100 fw-semibold text-uppercase">{{ elem.text }}</span>
-        </div>
-      </div>
+      <NavigationBarComponent />
     </div>
   </header>
 </template>
 
 <script>
-  import { menuData } from '../../data/data';
-  import LogoComponent from '../single/LogoCOmponent.vue';
+  import LogoComponent from '../single/LogoComponent.vue';
+  import NavigationBarComponent from '../single/NavigationBarComponent.vue';
   export default {
     name: "HeaderComponent",
     components:{
-      LogoComponent
-    },
-    data(){
-      return{
-        menuElements: menuData
-      }
+      LogoComponent,
+      NavigationBarComponent
     }
   }
 </script>
 
 <style lang="scss" scoped>
-  @import 'bootstrap/scss/bootstrap';
+  @use 'bootstrap/scss/bootstrap' as *;
   header{
     height: 15vh;
-  }
-  #headerMenu{
-    line-height: 113px;
-  }
-  .headerMenuElement{
-    cursor: pointer;
-    border-bottom: 5px solid transparent;
-    transition: all 0.1s linear;
-    height: 20px;
-  }
-  .active, .headerMenuElement:hover{
-    color: $primary;
-    border-bottom: 5px solid $primary;
   }
 </style>
---
+++ src/components/single/NavigationBarComponent.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="navigationBar" class="h-100 d-flex justify-content-between align-items-center gap-4">
+    <div class="navigationBarElement h-100" v-for="(elem,index) in menuElements" :class="{'active' : elem.isActive}">
+      <span class="h-100 fw-semibold text-uppercase">{{ elem.text }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+  import { menuData } from '../../data/data';
+  export default {
+    name: "NavigationBarComponent",
+    data(){
+      return{
+        menuElements: menuData
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  @use 'bootstrap/scss/bootstrap' as *;
+  #navigationBar{
+    line-height: 113px;
+  }
+  .navigationBarElement{
+    cursor: pointer;
+    border-bottom: 5px solid transparent;
+    transition: all 0.1s linear;
+    height: 20px;
+  }
+  .active, .navigationBarElement:hover{
+    color: $primary;
+    border-bottom: 5px solid $primary;
+  }
+</style>
