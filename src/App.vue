be294a452634f3c9ab7ed4118bbe8afd5382b9d5
totarochristian <totaro.christian.18@gmail.com>
2023-04-13 16:00:13 +0200

    Import the 3 component created in the App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script setup>
-
+  import HeaderComponent from "./components/HeaderComponent.vue";
+  import MainComponent from "./components/MainComponent.vue";
+  import FooterComponent from "./components/FooterComponent.vue";
 </script>
 
 <template>
