8429ba05beef87d7d96b2459576b33771fde0c3b
totarochristian <totaro.christian.18@gmail.com>
2023-04-13 17:19:47 +0200

    Define the main component content

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -1,12 +1,19 @@
 <template>
-  <main class="bg-custom-black">
-
+  <main class="w-100 h-100 bg-custom-black text-white d-flex justify-content-center align-items-center">
+    <div class="w-75 h-100 d-flex justify-content-between align-items-center">
+      <h2>{{ title }}</h2>
+    </div>
   </main>
 </template>
 
 <script>
   export default {
-    name: "MainComponent"
+    name: "MainComponent",
+    data(){
+      return{
+        title: "--> Content goes here <--"
+      }
+    }
   }
 </script>
 
