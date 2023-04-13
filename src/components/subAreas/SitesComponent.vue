a70ad579e57b6f781c41741c58eb6542e3f3a5f0
totarochristian <totaro.christian.18@gmail.com>
2023-04-13 18:34:52 +0200

    Add links in the sites component template

--- src/components/subAreas/SitesComponent.vue
+++ src/components/subAreas/SitesComponent.vue
@@ -1,18 +1,32 @@
 <template>
-  <div id="sites" class="w-100 bg-success">
-
+  <div id="sites" class="w-100 h-100 text-white d-flex justify-content-center align-items-center overflow-y-hidden">
+    <div class="w-75 h-100 d-flex justify-content-between align-items-center gap-4 overflow-y-hidden">
+      <div class="footerSitesElements w-50 d-flex flex-column align-items-start" v-for="(elem,index) in sitesElements">
+        <h4 class="h-100 fw-semibold text-uppercase">{{ elem.title }}</h4>
+        <div class="footerSitesLinks d-flex flex-column">
+          <a :href="link.link" v-for="link in elem.links">{{ link.text }}</a>
+        </div>
+      </div>
+      <img src="../../assets/images/dc-logo-bg.png" alt="Logo DC">
+    </div>
   </div>
 </template>
 
 <script>
+  import { sitesData } from '../../data/data';
   export default {
-    name: "SitesComponent"
+    name: "SitesComponent",
+    data(){
+      return{
+        sitesElements: sitesData
+      }
+    }
   }
 </script>
 
 <style lang="scss" scoped>
   #sites{
-    min-height: 40vh;
+    height: 40vh;
     background-image: url("../../assets/images/footer-bg.jpg");
     background-size: cover;
   }
