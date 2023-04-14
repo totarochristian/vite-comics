434b82e11250695311409aa97ee08bfe04214c51
totarochristian <totaro.christian.18@gmail.com>
2023-04-14 12:32:05 +0200

    Update the App component installing the sub components correctly

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,22 @@
-<script setup>
-  import HeaderComponent from "./components/mainAreas/HeaderComponent.vue";
-  import MainComponent from "./components/mainAreas/MainComponent.vue";
-  import FooterComponent from "./components/mainAreas/FooterComponent.vue";
-</script>
-
 <template>
   <HeaderComponent />
   <MainComponent />
   <FooterComponent />
 </template>
+
+<script>
+  import HeaderComponent from "./components/mainAreas/HeaderComponent.vue";
+  import MainComponent from "./components/mainAreas/MainComponent.vue";
+  import FooterComponent from "./components/mainAreas/FooterComponent.vue";
+  export default {
+    name: "App",
+    components:{
+      HeaderComponent,
+      MainComponent,
+      FooterComponent
+    }
+  }
+</script>
 
 <style scoped>
 
