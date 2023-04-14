26b3318cdff4c0f41a644fce7c018011b88b2b6d
totarochristian <totaro.christian.18@gmail.com>
2023-04-14 13:25:42 +0200

    Define and empty comics component and call it inside the main component (remove empty title component)

--- src/components/mainAreas/MainComponent.vue
+++ src/components/mainAreas/MainComponent.vue
@@ -1,17 +1,17 @@
 <template>
   <main class="w-100 h-100 bg-custom-black text-white d-flex justify-content-center align-items-center">
     <div class="w-75 h-100 d-flex justify-content-between align-items-center">
-      <EmptyTitleComponent />
+      <ComicsComponent />
     </div>
   </main>
 </template>
 
 <script>
-  import EmptyTitleComponent from '../subAreas/main/EmptyTitleComponent.vue';
+  import ComicsComponent from '../subAreas/main/ComicsComponent.vue';
   export default {
     name: "MainComponent",
     components:{
-      EmptyTitleComponent
+      ComicsComponent
     }
   }
 </script>
---
+++ src/components/subAreas/main/ComicsComponent.vue
@@ -0,0 +1,13 @@
+<template>
+  Bla bla
+</template>
+
+<script>
+  export default {
+    name: "ComicsComponent"
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
