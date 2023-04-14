26cc0c509f14ead1531ffbda7216e228f5dd1bbb
totarochristian <totaro.christian.18@gmail.com>
2023-04-14 15:50:45 +0200

    Add price and type to the comic component + the show effect when mouse on hover of the component

--- src/components/single/ComicComponent.vue
+++ src/components/single/ComicComponent.vue
@@ -2,6 +2,8 @@
   <div :id="'comic_'+index" class="comic d-flex flex-column">
     <div class="comicImage">
       <img :src="comicData.thumb" :alt="comicData.series" class="w-100 h-100">
+      <span class="type text-uppercase fw-bold">{{ comicData.type }}</span>
+      <span class="price fs-1 fw-bold">{{ comicData.price }}</span>
     </div>
     <p class="w-100 text-uppercase pt-3">{{ comicData.series }}</p>
   </div>
@@ -22,11 +24,36 @@
   .comic{
     width: $squareSize;
     .comicImage{
+      position: relative;
       width: 100%;
       height: $squareSize;
       img{
         object-fit: cover;
       }
+      .type,.price{
+        visibility: hidden;
+        position: absolute;
+      }
+      .type{
+        top: 10px;
+        right: 10px;
+      }
+      .price{
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%,-50%);
+      }
     }
+    &:hover{
+        cursor: pointer;
+        .comicImage{
+          img{
+            filter: brightness(50%);
+          }
+          .type,.price{
+            visibility: visible;
+          }
+        }
+      }
   }
 </style>
