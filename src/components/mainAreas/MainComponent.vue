30721064a2a1de686e9fe560dd80a77e08c6e4a0
totarochristian <totaro.christian.18@gmail.com>
2023-04-14 13:21:50 +0200

    Add empty title component and call it in the main component

--- src/components/mainAreas/MainComponent.vue
+++ src/components/mainAreas/MainComponent.vue
@@ -1,18 +1,17 @@
 <template>
   <main class="w-100 h-100 bg-custom-black text-white d-flex justify-content-center align-items-center">
     <div class="w-75 h-100 d-flex justify-content-between align-items-center">
-      <h2>{{ title }}</h2>
+      <EmptyTitleComponent />
     </div>
   </main>
 </template>
 
 <script>
+  import EmptyTitleComponent from '../subAreas/main/EmptyTitleComponent.vue';
   export default {
     name: "MainComponent",
-    data(){
-      return{
-        title: "--> Content goes here <--"
-      }
+    components:{
+      EmptyTitleComponent
     }
   }
 </script>
